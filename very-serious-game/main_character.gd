c5783ef7a7bba5cabc0aa3ddc6f7853295343eda
equo-art <rsacheri@gmail.com>
2026-06-19 14:02:42 -0400

    he spins

---
+++ very-serious-game/main_character.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotation += 2 * delta
+
+func _input(event):
+	if event.is_action_pressed("grab"):
+		print("my_action occurred!")
