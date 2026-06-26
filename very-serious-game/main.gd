8446d97e401a57c91637c9d2b7bccce240d58e02
equo-art <rsacheri@gmail.com>
2026-06-26 12:54:30 -0400

    music now loops

--- very-serious-game/main.gd
+++ very-serious-game/main.gd
@@ -22,9 +22,10 @@ func _ready() -> void:
 	#_lost_life()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
+ # Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (!$%music.playing):
+		$music.play()
 
 
 ############################################################
